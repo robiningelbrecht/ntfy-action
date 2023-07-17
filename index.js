7cd6bbf8e49329f063112e850e80e18168a75a06
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-07-17 19:19:55 +0200

    Start testing action

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const isTrue = (variable) => {
 
 async function run() {
     try {
-        const context = github.context;
+        const context = github.context.repo;
         // Possible values are success, failure, or cancelled.
         const jobStatus = core.getInput('job_status');
 
